bf463487422c3175cd42e39d867449a7599bfc53
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-05-07 13:46:45 -0600

    Update
    Imported login view and signup view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,26 +1,47 @@
 import {useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../Signup-View/SignupView";
 import { useState } from "react";
 import { useEffect } from "react";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([])
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(null);
+    const [token, setToken] =useState(null);
 
     useEffect(() => {
-        fetch("https://git.heroku.com/horban-movie-api.git")
+        if (!token) {
+            return;
+        }
+        
+        fetch("https://git.heroku.com/horban-movie-api.git/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
         .then((response) => response.json())
         .then((data) => {
-            const moviesFromApi = data.docs.map((doc) => {
-                return {
-                    id: doc.key,
-                    title: doc.title
-                };
-            });
-            setMovies(moviesFromApi);
-    });
-    }, []);
+            console.log(data);
+        });
+    }, [token]);
+
+
+    if (!user) {
+        return (
+            <>
+                <LoginView
+                 onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }} />
+            or
+            <SignupView />
+            </>
+            );
+        }
 
     if (selectedMovie) { 
         return (
@@ -43,6 +64,7 @@ export const MainView = () => {
                     }}
                     />
             ))}
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
         </div>
     );
 };
