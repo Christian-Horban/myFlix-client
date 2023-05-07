15850bcc477108f3ca122d19bfdd8f0007ff2586
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-05-07 13:46:19 -0600

    Create SignupView.jsx

---
+++ src/components/Signup-View/SignupView.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+export const SignupView = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    cosnt [birthday, serBirthday] = useState("");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+        Username: username,
+        Password: password,
+        email: email,
+        Birthday: birthday
+    };
+
+    fetch("https://git.heroku.com/horban-movie-api.git/users", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        if (response.ok) {
+            alert("Signup successful");
+            window.location.replace("/login");
+        } else {
+            alert("Signup failed");
+        }
+    });
+};
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                minLength="3"
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                Email:
+                <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                Birthday:
+                <input
+                type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required
+                />
+            </label>
+                <button type="submit">Submit</button>
+        </form>
+    );
+};
