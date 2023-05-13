1fa7eaf82b5a0290cd19d8ad9b119cacb7066183
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-05-13 12:34:22 -0700

    Update movie-card.jsx
    Imported proptypes and react elements

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,27 @@
+import react from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
         <Card>
-          <Card.Img variant="top" src={book.image} />
+          <Card.Img variant="top" src={movie.image} />
           <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-            <Card.Text>{book.author}</Card.Text>
-            <Button onClick={() => onBookClick(book)} variant="link">
-              Open
-            </Button>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.description}</Card.Text>
+            <Link to={`/movies/${encodedURIComponent(movie.id)}`}>
+                <Button variant="link">Open</Button>
+            </Link>
           </Card.Body>
         </Card>
       );
+    };
+
+    MovieCard.PropTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired,
+            director: PropTypes.string
+        }).isRequired
     };
