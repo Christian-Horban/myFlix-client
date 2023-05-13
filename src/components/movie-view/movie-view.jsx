157062407f78eff5066258ee455d367351aabee6
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-05-13 12:34:34 -0700

    Update movie-view.jsx
    Imported proptypes and react elements

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,70 +1,113 @@
-import PropTypes from 'prop-types';
-import { Button, Card, Row, Col } from 'react-bootstrap';
-import './movie-view.scss';
+import PropTypes from "prop-types";
+import { Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router"
+import { MovieCard } from "../movie-card/movie-card";
+import { useEffect, useState } from "react";
 
-export const MovieView = ({ movieData, onBackClick }) => {
+export const MovieView = ({ movies, user, token, updateUser }) => {
+    const { movieId } = useParams();
+    const movie = movies.find(m => m.id === movieId);
+    const similarMovies = movies.filter(movie => movie.genre === movie.genre ? true : false)
+
+    const [isFavorite, setIsFavorite] = useState(user.favoriteMovies.includes(movie.id));
+
+    useEffect(() => {
+        setIsFavorite(user.favoriteMovies.includes(movie.id));
+        window.scrollTo(0, 0);
+    }, [movieId])
+
+    const addFavorite = () => {
+        fetch(`https://git.heroku.com/horban-movie-api.git/users/${user.username}/movies/${movieId}`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Failed");
+                return false;
+            }
+        })
+        .then(user => {
+            if (user) {
+                alert("Successfully added to favorites");
+                setIsFavorite(true);
+                updateUser(user);
+            }
+        })
+        .catch(e => {
+            alert(e);
+        });
+    }
+
+    const removeFavorite = () => {
+        fetch(`https://git.heroku.com/horban-movie-api.git/users/${user.username}/movies/${movieId}`, {
+            method: "DELETE",
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Failed");
+                return false;
+            }
+        })
+        .then(user => {
+            if (user) {
+                alert("Successfully deleted from favorites");
+                setIsFavorite(false);
+                updateUser(user);
+            }
+        })
+        .catch(e => {
+            alert(e);
+        });
+    }
+
     return (
-<Row className='d-flex flex-row-reverse p-3'>
-      <Col md={5} className='text-center text-md-end'>
-        <img
-          src={movieData.image}
-          alt={`Poster for ${movieData.title}`}
-          className='img-fluid h-100 w-auto movie-view-img'
-        />
-      </Col>
-      <Col md={7} className='d-flex flex-column'>
-        <Row className='d-flex flex-row  justify-content-between'>
-          <Col md={9} className='d-flex flex-column'>
-            <h3 className='my-0'>
-              <span>Title: </span>
-              <span>{movieData.title}</span>
-            </h3>
-            <h5 className='mt-1 text-left text-muted'>
-              <span>Director: </span>
-              <span>{movieData.director.name}</span>
-            </h5>
-          </Col>
-
-          <Col md={3} className='align-self-end mb-2 text-end'>
-            <span>Genre: </span>
-            <span className='fw-bolder'>{movieData.genre.name}</span>
-          </Col>
-        </Row>
-        <div className='mt-md-5 mb-4'>
-          <div className='text-decoration-underline mb-2'>Description: </div>
-          <span>{movieData.description}</span>
-        </div>
-        <div className='mt-auto text-end mb-md-4'>
-          <Button
-            onClick={() => {
-              onBackClick();
-            }}
-            variant='secondary'
-            size='lg'
-          >
-            Back
-          </Button>
-        </div>
-      </Col>
-    </Row>
-  );
+        <>
+            <Col md={12}>
+                <div className="text-light">
+                    <img className="float-start me-3 mb-2" src={movie.image} alt="Movie Cover Image" />
+                    <h2>{movie.title} ({movie.year})</h2>
+                    <p>{movie.description}</p>
+                    <h4>Actors:</h4>
+                    <h5>{movie.actors.join(" - ")}</h5>
+                    <h4>Genre: </h4>
+                    <h5>{movie.genre}</h5>
+                    <p>{movie.genredescription}</p>
+                    <h4>Director: </h4>
+                    <h5>{movie.director} ({movie.directorbirth.slice(0, 10)}{movie.directordeath ? ` - ${movie.directordeath.slice(0, 10)}` : ""})</h5>
+                    <p>{movie.directorbio}</p>
+                    <Link to={"/"}>
+                        <Button variant="primary">Back</Button>
+                    </Link>
+                    {isFavorite ? 
+                        <Button variant="danger" className="ms-2" onClick={removeFavorite}>Remove from favorites</Button>
+                        : <Button variant="success" className="ms-2" onClick={addFavorite}>Add to favorites</Button>
+                    }                   
+                    <h3 className="mt-3 mb-3 text-light">Similar movies:</h3>
+                </div>
+            </Col> 
+            {similarMovies.map(movie => (
+                <Col className="mb-4" key={movie.id} xl={2} lg={3} md={4} xs={6}>
+                    <MovieCard movie={movie} />
+                </Col>
+            ))}
+        </>
+    );
 };
 
 MovieView.propTypes = {
-  movieData: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    director: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      bio: PropTypes.string.isRequired,
-      birth: PropTypes.string.isRequired,
-      death: PropTypes.string,
-    }).isRequired,
-    genre: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }).isRequired,
-    image: PropTypes.string.isRequired,
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired,
+    movies: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+    }).isRequired)
 };
