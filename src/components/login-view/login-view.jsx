a6b54c1b28e31773222c3b84c486d2625cf08d6b
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-06-05 11:54:23 -0600

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -16,7 +16,7 @@ export const LoginView = ({ onLoggedIn }) => {
       password,
     };
 
-    fetch('https://myflix-horban.netlify.app/login', {
+    fetch('https://horban-movie-api.herokuapp.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
