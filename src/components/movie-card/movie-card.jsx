63c17e504ae7d4e7e1b0ca8977843f8b019879e0
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-08-19 12:03:27 -0600

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -20,8 +20,8 @@ export const MovieCard = ({ movie }) => {
 
     MovieCard.propTypes = {
         movie: PropTypes.shape({
-            title: PropTypes.string.isRequired,
+            Title: PropTypes.string.isRequired,
             image: PropTypes.string.isRequired,
-            director: PropTypes.string
+            Director: PropTypes.string
         }).isRequired
     };
