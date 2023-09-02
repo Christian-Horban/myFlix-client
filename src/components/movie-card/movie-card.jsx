5f1764c7b47e7106e3b743d355a00bcbfbba460f
Christian Horban <116985183+Christian-Horban@users.noreply.github.com>
2023-09-02 01:50:34 -0600

    added img file to card and styling to card view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,14 +4,39 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
+
+  const cardBodyStyle = {
+    textAlign: 'center',
+  };
+
+  const cardImageContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '300px', // Set the desired height for the image container
+  };
+
+  const imageStyle = {
+    width: '200px', // Set a fixed width for the images
+    height: 'auto', // Maintain aspect ratio
+    maxWidth: '100%', // Ensure the image doesn't exceed its container
+  };
+
+  const buttonStyle = {
+    fontSize: '1.2rem',
+  };
+
+
     return (
         <Card>
-          <Card.Img variant="top" src={movie.image} />
-          <Card.Body>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>{movie.Description}</Card.Text>
+          <div style={cardImageContainerStyle}>
+        <Card.Img variant="top" src={movie.image} style={imageStyle} />
+      </div>
+          <Card.Body style={cardBodyStyle} > 
+            <Card.Title>{movie.title}</Card.Title>
+            {/* <Card.Text>{movie.description}</Card.Text> */}
             <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-                <Button variant="link">Open</Button>
+                <Button variant="link" style={buttonStyle} >Open</Button>
             </Link>
           </Card.Body>
         </Card>
