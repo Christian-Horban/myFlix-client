7dd884a5fc17db06a6914b9817869391181134b6
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-22 22:48:13 -0600

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router";
 import { MovieCard } from "../movie-card/movie-card";
 import { useEffect, useState } from "react";
-import  "./movie-view.scss"
+import "./movie-view.scss";
 
 export const MovieView = ({ movies, user, token, updateUser }) => {
   console.log("MOVIES", movies);
@@ -12,38 +12,39 @@ export const MovieView = ({ movies, user, token, updateUser }) => {
   const movie = movies.find((m) => m._id === movieId);
 
   return (
-    <Col md={12}>
-      <Card className="d-flex">
-        <div className="img-container">
-          <Card.Img
-            src={movie.image}
-            alt="Movie Cover Image"
-          />
-        </div>
-        <div className="movie-info">
-          <Card.Body>
-            <Card.Title className="text-center">{movie.title}</Card.Title>
-            <Card.Title className="text-center">Description:</Card.Title>
-            <Card.Text className="text-center">{movie.description}</Card.Text>
-            <Card.Subtitle className="text-center">
-              Genre: {movie.genre.name}
-            </Card.Subtitle>
-            <Card.Text className="text-center">
-              {movie.genre.description}
-            </Card.Text>
-            <Card.Subtitle className="text-center">
-              Director: {movie.director.name}
-            </Card.Subtitle>
-            <Card.Text className="text-center">{movie.director.bio}</Card.Text>
-            <div className="text-center">
-              <Link to={"/"}>
-                <Button variant="primary">Back</Button>
-              </Link>
-            </div>
-          </Card.Body>
-        </div>
-      </Card>
-    </Col>
+    <div className="movie-view-container">
+      <Col md={12}>
+        <Card className="d-flex">
+          <div className="img-container">
+            <Card.Img src={movie.image} alt="Movie Cover Image" />
+          </div>
+          <div className="movie-info">
+            <Card.Body>
+              <Card.Title className="text-center">{movie.title}</Card.Title>
+              <Card.Title className="text-center">Description:</Card.Title>
+              <Card.Text className="text-center">{movie.description}</Card.Text>
+              <Card.Subtitle className="text-center">
+                Genre: {movie.genre.name}
+              </Card.Subtitle>
+              <Card.Text className="text-center">
+                {movie.genre.description}
+              </Card.Text>
+              <Card.Subtitle className="text-center">
+                Director: {movie.director.name}
+              </Card.Subtitle>
+              <Card.Text className="text-center">
+                {movie.director.bio}
+              </Card.Text>
+              <div className="text-center">
+                <Link to={"/"}>
+                  <Button variant="primary">Back</Button>
+                </Link>
+              </div>
+            </Card.Body>
+          </div>
+        </Card>
+      </Col>
+    </div>
   );
 };
 
