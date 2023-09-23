fad643198c3e5f1ae0169cc8bc9da4c7323f35bf
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-22 19:51:08 -0600

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,94 +9,82 @@ export const MovieView = ({ movies, user, token, updateUser }) => {
     console.log("MOVIES", movies);
     const { movieId } = useParams();
     const movie = movies.find(m => m._id === movieId);
-    // const [isFavorite, setIsFavorite] = useState(
-    //     user && movie && user.FavoriteMovies.includes(movie._id)
-    //   );
-      
-    
-    //   useEffect(() => {
-    //     setIsFavorite(
-    //       user && movie && user.FavoriteMovies.some((fm) => fm == movie._id)
-    //     );
-    //   }, [user, movie]);
-
-
-
-    //   const handleToggleFavorite = () => {
-    //     if (!user || !movie) return;
-    
-    //     fetch(
-    //       `https://horban-movie-api.herokuapp.com/users/${user.Username}/FavoriteMovies/${movie._id}`,
-    //       {
-    //         method: isFavorite ? 'DELETE' : 'POST',
-    //         headers: {Authorization: `Bearer ${token}`},
-    //       }
-    //     )
-    //       .then((response) => {
-    //         if (!response.ok) {
-    //           throw response;
-    //         }
-    //         return response.json();
-    //       })
-    //       .then((updatedUserResponse) => {
-    //         setUser(updatedUserResponse.user);
-    //         setIsFavorite(
-    //           updatedUserResponse.user.FavoriteMovies.some((fm) => fm == movie._id)
-    //         );
-    //       })
-    //       .catch((error) => {
-    //         const contentType = error.headers.get('content-type');
-    //         if (contentType && contentType.indexOf('application/json') !== -1) {
-    //           error
-    //             .json()
-    //             .then((errorMessage) =>
-    //               alert(`An error occurred: ${errorMessage}`)
-    //             );
-    //         } else {
-    //           error.text().then((errorMessage) => alert(errorMessage));
-    //         }
-    //       });
-    //   };
-
 
     return (
-        <>
-            <Col md={12}>
-                <div /*className="text-light"*/>
+        <Col md={12}>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center">{movie.title}</Card.Title>
+              <Card.Img
+                src={movie.image}
+                alt="Movie Cover Image"
+                className="mx-auto d-block"
+                style={{ maxWidth: "100%" }}
+              />
+              <Card.Title className="text-center">Description:</Card.Title>
+              <Card.Text className="text-center">{movie.description}</Card.Text>
+              <Card.Subtitle className="text-center">
+                Genre: {movie.genre.name}
+              </Card.Subtitle>
+              <Card.Text className="text-center">
+                {movie.genre.description}
+              </Card.Text>
+              <Card.Subtitle className="text-center">
+                Director: {movie.director.name}
+              </Card.Subtitle>
+              <Card.Text className="text-center">
+                {movie.director.bio}
+              </Card.Text>
+              <div className="text-center">
+                <Link to={"/"}>
+                  <Button variant="primary">Back</Button>
+                </Link>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      );
+    };
+
+
+//     return (
+//         <>
+//             <Col md={12}>
+//                 <div /*className="text-light"*/>
                     
-                    <h2 style={{ textAlign: 'center' }}>{movie.title}</h2>
-                    <img
-                        style={{
-                                display: 'block',
-                                margin: '0 auto',
-                                width: '100%', // Set the width to 100% for uniform size
-                                height: 'auto', // Maintain aspect ratio
-                                padding: '3px', // Add 3px padding on all sides
-                            }}
-                            src={movie.image}
-                            alt="Movie Cover Image"
-        />
-                    <h3 style={{ textAlign: 'center' }}> Description: </h3>
-                    <p style={{ textAlign: 'center' }}>{movie.description}</p>
-                    <h4 style={{ textAlign: 'center' }}>Genre: {movie.genre.name}</h4>
-                    <p style={{ textAlign: 'center' }}>{movie.genre.description}</p>
-                    <h4 style={{ textAlign: 'center' }}>Director: {movie.director.name}</h4>
-                    <p style={{ textAlign: 'center' }}>{movie.director.bio}</p>
-                    <Link to={"/"}>
-                        <Button variant="primary">Back</Button>
-                    </Link>
-                    {/* <Button
-                  variant={isFavorite ? 'danger' : 'success'}
-                  onClick={handleToggleFavorite}
-                  style={{marginTop: '6px', marginLeft: '6px'}}
-                >
-                  {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
-                </Button> */}
-                </div>
-            </Col> 
-        </>
-    );
-};
+//                     <h2 style={{ textAlign: 'center' }}>{movie.title}</h2>
+//                     <img
+//                         style={{
+//                                 display: 'block',
+//                                 margin: '0 auto',
+//                                 width: '100%', // Set the width to 100% for uniform size
+//                                 height: 'auto', // Maintain aspect ratio
+//                                 padding: '3px', // Add 3px padding on all sides
+//                             }}
+//                             src={movie.image}
+//                             alt="Movie Cover Image"
+//         />
+//                     <h3 style={{ textAlign: 'center' }}> Description: </h3>
+//                     <p style={{ textAlign: 'center' }}>{movie.description}</p>
+//                     <h4 style={{ textAlign: 'center' }}>Genre: {movie.genre.name}</h4>
+//                     <p style={{ textAlign: 'center' }}>{movie.genre.description}</p>
+//                     <h4 style={{ textAlign: 'center' }}>Director: {movie.director.name}</h4>
+//                     <p style={{ textAlign: 'center' }}>{movie.director.bio}</p>
+//                     <Link to={"/"}>
+//                         <Button variant="primary">Back</Button>
+//                     </Link>
+//                     {/* <Button
+//                   variant={isFavorite ? 'danger' : 'success'}
+//                   onClick={handleToggleFavorite}
+//                   style={{marginTop: '6px', marginLeft: '6px'}}
+//                 >
+//                   {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+//                 </Button> */}
+//                 </div>
+//             </Col> 
+//         </>
+//     );
+// };
 
 MovieView.propTypes = {
     movie: PropTypes.arrayOf(PropTypes.shape({
