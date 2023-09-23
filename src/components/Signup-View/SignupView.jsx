4932a58addf5dc83af9f6c76b5cb7346dd014128
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-23 00:53:00 -0600

    Update SignupView.jsx

--- src/components/Signup-View/SignupView.jsx
+++ src/components/Signup-View/SignupView.jsx
@@ -44,48 +44,6 @@ export const SignupView = () => {
   };
 
   return (
-    // <Form onSubmit={handleSubmit}>
-    //   <Form.Group controlId="formUsername">
-    //     <Form.Label>Username:</Form.Label>
-    //     <Form.Control
-    //       type="text"
-    //       value={username}
-    //       onChange={(e) => setUsername(e.target.value)}
-    //       required
-    //       minLength="6"
-    //     />
-    //   </Form.Group>
-    //   <Form.Group controlId="formPassword">
-    //     <Form.Label>Password:</Form.Label>
-    //     <Form.Control
-    //       type="text"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       required
-    //       minLength="6"
-    //     />
-    //   </Form.Group>
-    //   <Form.Group controlId="formEmail">
-    //     <Form.Label>Email:</Form.Label>
-    //     <Form.Control
-    //       type="text"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       required
-    //     />
-    //   </Form.Group>
-    //   <Form.Group controlId="formBirthday">
-    //     <Form.Label>Birthday:</Form.Label>
-    //     <Form.Control
-    //       type="date"
-    //       value={birthday}
-    //       onChange={(e) => setBirthday(e.target.value)}
-    //       required
-    //     />
-    //   </Form.Group>
-    //   <Button type="submit">Submit</Button>
-    // </Form>
-
     <Container>
       <Row>
         <Col>
