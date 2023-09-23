19fae85909bf31dd77eb3cc9afa4687427838893
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-23 00:46:21 -0600

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -89,60 +89,62 @@ export const LoginView = ({ onLoggedIn }) => {
     <Container>
       <Row>
         <Col>
-        <CardGroup>
-          <Card>
-            <Card.Title>Login</Card.Title>
-          <Form onSubmit={handleSubmit} className="login-form">
-            <FormGroup className="mb=3" controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <FormControl
-                type="test"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                minLength="3"
-              />
-              <Form.Text className="text-muted">
-                We will never share your email with anyone else.
-              </Form.Text>
-            </FormGroup>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title>Login</Card.Title>
+                <Form onSubmit={handleSubmit} className="login-form">
+                  <FormGroup className="mb=3" controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <FormControl
+                      type="text"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                      minLength="3"
+                    />
+                    <Form.Text className="text-muted">
+                      We will never share your email with anyone else.
+                    </Form.Text>
+                  </FormGroup>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-            <div className="mt-3">
-              <p
-                style={{
-                  fontWeight: "bold",
-                  padding: "3px",
-                  textAlign: "center",
-                }}
-              >
-                Not a member yet?{" "}
-                <Link
-                  style={{
-                    fontWeight: "bold",
-                    padding: "3px",
-                    textAlign: "center",
-                  }}
-                  to="/signup"
-                >
-                  Sign up!
-                </Link>
-              </p>
-            </div>
-          </Form>
-          </Card>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                  <div className="mt-3">
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        padding: "3px",
+                        textAlign: "center",
+                      }}
+                    >
+                      Not a member yet?{" "}
+                      <Link
+                        style={{
+                          fontWeight: "bold",
+                          padding: "3px",
+                          textAlign: "center",
+                        }}
+                        to="/signup"
+                      >
+                        Sign up!
+                      </Link>
+                    </p>
+                  </div>
+                </Form>
+              </Card.Body>
+            </Card>
           </CardGroup>
         </Col>
       </Row>
