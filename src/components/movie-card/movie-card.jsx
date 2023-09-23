60b6c7353531355df00ab87d7187ecaaad635fb0
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-22 21:57:46 -0600

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,30 +1,29 @@
 import react from "react";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, user, setUser, token }) => {
-
   const cardBodyStyle = {
-    textAlign: 'center',
+    textAlign: "center",
   };
 
   const cardImageContainerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '300px', // Set the desired height for the image container
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "300px", // Set the desired height for the image container
   };
 
   const imageStyle = {
-    width: '200px', // Set a fixed width for the images
-    height: 'auto', // Maintain aspect ratio
-    maxWidth: '100%', // Ensure the image doesn't exceed its container
+    width: "200px", // Set a fixed width for the images
+    height: "auto", // Maintain aspect ratio
+    maxWidth: "100%", // Ensure the image doesn't exceed its container
   };
 
   const buttonStyle = {
-    fontSize: '1.2rem',
+    fontSize: "1.2rem",
   };
 
   const [isFavorite, setIsFavorite] = useState(
@@ -37,25 +36,24 @@ export const MovieCard = ({ movie, user, setUser, token }) => {
 
   const handleToggleFavorite = async () => {
     if (!user || !movie) {
-      console.log('User or movie is undefined.');
+      console.log("User or movie is undefined.");
       return;
     }
 
     if (!user.FavoriteMovies) {
-      console.log('FavoriteMovies is undefined in user.');
+      console.log("FavoriteMovies is undefined in user.");
       return;
     }
 
     let response;
-    
 
     if (isFavorite) {
       // Remove the movie from the favorites
       response = await fetch(
         `https://horban-movie-api.herokuapp.com/users/${user.Username}/FavoriteMovies/${movie._id}`,
         {
-          method: 'DELETE',
-          headers: {Authorization: `Bearer ${token}`},
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${token}` },
         }
       );
     } else {
@@ -63,69 +61,79 @@ export const MovieCard = ({ movie, user, setUser, token }) => {
       response = await fetch(
         `https://horban-movie-api.herokuapp.com/users/${user.Username}/FavoriteMovies/${movie._id}`,
         {
-          method: 'POST',
-          headers: {Authorization: `Bearer ${token}`},
+          method: "POST",
+          headers: { Authorization: `Bearer ${token}` },
         }
       );
     }
 
     if (response.ok) {
       const userResponse = await response.json();
-console.log('userResponse:', userResponse);
-setUser(userResponse.user);
+      console.log("userResponse:", userResponse);
+      setUser(userResponse.user);
 
-  
       if (userResponse.user.FavoriteMovies) {
         setIsFavorite(
           userResponse.user.FavoriteMovies.some((fm) => fm == movie.id)
         );
       } else {
-        console.log('FavoriteMovies is undefined in updated user.');
+        console.log("FavoriteMovies is undefined in updated user.");
       }
     } else {
-      console.log('Failed to update favorite movies.');
+      console.log("Failed to update favorite movies.");
     }
   };
 
-    
-    return (
-        <Card className="card">
-        
-          <Card.Img
-                variant="top"
-                src={movie.image}
-                alt="Movie Cover Image"
-                className="card-img-container"
-                style={{ maxWidth: "100%" }}
-              />       
-          <Card.Body className='card-body' > 
-            <Card.Title className='card-title'>{movie.title} </Card.Title>
-            {/* <Card.Text>{movie.description}</Card.Text> */}
-            <div className="d-flex justify-content-center mb-3">
-            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-                <Button variant="primary" className="btn open-button">Open</Button>
-            </Link>
-            <div className="mx-2"></div>
-            <Button
-          variant={isFavorite ? 'danger' : 'success'}
-          size="sm"
-          onClick={handleToggleFavorite}
+  return (
+    <Card className="card">
+      <Card.Img
+        variant="top"
+        src={movie.image}
+        alt="Movie Cover Image"
+        className="card-img-container"
+        style={{ maxWidth: "100%" }}
+      />
+      <Card.Body className="card-body">
+        <Card.Title
+          style={{
+            fontSize: "1.25rem",
+            marginTop: "10px",
+            height: "2.5rem", // Set a fixed height for the title container
+            overflow: "hidden", // Hide any overflowing text
+            whiteSpace: "normal", // Allow text to wrap to the next line
+            textAlign: "center", // Center-align the title text
+          }}
         >
-          {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
-        </Button> 
+          {movie.title}
+        </Card.Title>
+        {/* <Card.Text>{movie.description}</Card.Text> */}
+        <div className="d-flex justify-content-center mb-3">
+          <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+            <Button variant="primary" className="btn open-button">
+              Open
+            </Button>
+          </Link>
+          <div className="mx-2"></div>
+          <Button
+            variant={isFavorite ? "danger" : "success"}
+            size="sm"
+            onClick={handleToggleFavorite}
+          >
+            {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+          </Button>
         </div>
-          </Card.Body>
-        </Card>
-      );
-    };
+      </Card.Body>
+    </Card>
+  );
+};
 
-    MovieCard.propTypes = {
-        movie: PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            image: PropTypes.string.isRequired,
-            Director: PropTypes.string
-        }).isRequired,
-        user: PropTypes.object, 
-        setUser: PropTypes.func.isRequired, 
-        token: PropTypes.string.isRequired 
-    };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    Director: PropTypes.string,
+  }).isRequired,
+  user: PropTypes.object,
+  setUser: PropTypes.func.isRequired,
+  token: PropTypes.string.isRequired,
+};
